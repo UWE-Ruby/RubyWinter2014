0fd20341c65b71cc8e2bcfe7e764027e39c6121f
Neil Woodward <drlctr@gmail.com>
2014-01-12 19:51:06 -0800

    Commiting exercises for Practice

--- week1/exercises/rspec_spec.rb
+++ week1/exercises/rspec_spec.rb
@@ -77,15 +77,19 @@ describe "The Rspec ruby gem" do
   		# Fix the Failing Test
   		# Order of Operations is Please Excuse My Dear Aunt Sally:
   		# Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
-  		(1+2-5*6/2).should eq -6
+  		((1+2-5)*6/2).should eq -6
   	end
   	it "should count the characters in your name" do
-      pending
-    end
+          "Neil".should have(4).characters
+        end
   	
-  	it "should check basic math"
+  	it "should check basic math" do
+          (2+2).should eq 4
+        end
 
-  	it "should check basic spelling"
+  	it "should check basic spelling" do
+          "Neil".should include("ei")
+        end
 
   end
 
