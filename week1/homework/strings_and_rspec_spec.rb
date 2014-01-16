c40b45989db00ec7d8716da7729891b397379177
Kevin Young <kevin_young@intuit.com>
2014-01-16 10:15:28 -0800

    finished spec

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -8,23 +8,22 @@
 # 	(Hint: You should do the reading on Strings first)
 
 describe String do
-	context "When a string is defined" do
-		before(:all) do
-			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
-		end
-	 
-   	it "should be able to count the charaters" do
-         @my_string.should have(66).characters
-		end
+  context "When a string is defined" do
+    before(:all) do
+      @my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
+    end
+   	
+    it "should be able to count the charaters" do
+      @my_string.should have(66).characters
+    end
 
-      it "should be able to split on the . charater" do
-			result = @my_string.split('.')
-			result.should have(2).items	
-		end
+    it "should be able to split on the . charater" do
+      @my_string.split('.').should have(2).items
+    end
 
-		it "should be able to give the encoding of the string" do
-         @my_string.should eq (Encoding.find("UTF-8"))
-		end
-	end
+    it "should be able to give the encoding of the string" do
+      @my_string.encoding.should eq (Encoding.find("UTF-8"))
+    end
+  end
 end
 
