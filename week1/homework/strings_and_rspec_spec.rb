cdeab056839c7651cc4b1b27af461dc3bb30a31d
Jack Howland <jack.howl@gmail.com>
2014-01-16 14:38:33 -0800

    jack1:submit homework

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -13,13 +13,19 @@ describe String do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
 		it "should be able to count the charaters"
+      @my_string.count
+      # ruby count string characters
 		it "should be able to split on the . charater" do
+      # ruby split code
+      #
+      result = @my_string.split(.)
 			pending
-			result = #do something with @my_string here
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
 			pending 'helpful hint: should eq (Encoding.find("UTF-8"))'
+      # encoding string
+      @my_string.force_decoding("UTF-8").valid_encoding?
 		end
 	end
 end
