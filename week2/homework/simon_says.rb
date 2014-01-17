70ca1ea3771d4afe46ec3b8b68ebf53c867ce523
mattsface <spahmatthew@gmail.com>
2014-01-16 22:08:31 -0800

    added simon_says.rb

---
+++ week2/homework/simon_says.rb
@@ -0,0 +1,27 @@
+
+
+class SimonSays
+	def initialize(word)
+		@word = word
+	end
+
+	def echo
+		puts "#{SimonSays.word}"
+	end
+
+	def shout
+		puts "#{SimonSays.word.capitalize}"
+	end
+
+	def repeat
+
+	end
+
+	def start_of_word
+
+	end
+
+	def first_word
+
+	end
+end
