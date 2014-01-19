7401d4d355fc9466c9b34cb401a2abbf3d424f37
mattsface <spahmatthew@gmail.com>
2014-01-18 17:53:20 -0800

    updated simon_says.rb, removed the returns

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -2,26 +2,26 @@ module SimonSays
 
 	# return the string
 	def echo(string)
-		return "#{string}"
+		"#{string}"
 	end
 
 	# return string in all capitals 
 	def shout(string)
-		return "#{string.upcase}"
+		"#{string.upcase}"
 	end
 
 	# repeat the string a min of 2 times, unless specified 
 	def repeat(string, repeat=2)
-		return ("#{string} " * repeat).strip
+		("#{string} " * repeat).strip
 	end
 
 	# return the charaters starting from start of string to length
 	def start_of_word(string, length)
-		return "#{string[0, length]}"
+		"#{string[0, length]}"
 	end
 
 	# return first word in a string using a space has a delimiter 
 	def first_word(string)
-		return "#{string.split(" ")[0]}"
+		"#{string.split(" ")[0]}"
 	end
 end
