cba25955989e180ca8e193c9e3162b72b1fb0329
Renée De Voursney <renee@nird.us>
2014-01-23 21:07:32 -0800

    week3 in-class work

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -1,21 +1,23 @@
 module SimonSays
-	def echo(st)
-		st
-	end
-	
-	def shout(st)
-		st.upcase
-	end
 
-	def first_word(st)
-		st.split.first
-	end
+  def echo input
+    input
+  end
 
-	def start_of_word(st,i)
-		st[0...i]
-	end
-	
-	def repeat(st, t=2)
-		([st]*t).join(' ')
-	end
+  def shout input
+    input.upcase
+  end
+
+  def repeat input, n = 2
+    ([input] * n ).join(' ')
+  end
+
+  def start_of_word input, n
+    input.slice(0,n)
+  end
+
+  def first_word sentance
+    sentance.split.first
+  end
+
 end
