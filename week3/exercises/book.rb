5fe743ed6d95ece74690fc5bd0ab0ea4e11dc247
John <JohnRobertWade@gmail.com>
2014-01-25 15:54:30 -0800

    completed homework 3

--- week3/exercises/book.rb
+++ week3/exercises/book.rb
@@ -1,7 +1,21 @@
 class Book
 
-  def pages
-    
-  end
+	attr_accessor :pages, :title
+
+	@@library_count = 0
+
+	def self.library_count
+		@@library_count
+	end
+
+	def initialize pages = 0, title="N/A"
+		@pages = pages
+		@title = title
+		@@library_count +=1
+	end
+
+	def happy
+		"There are #{pages} happy pages in this book"
+	end
 
 end
