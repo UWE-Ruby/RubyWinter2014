b254a0e918fa8aa0369a6d300b6eec55c1127fd3
mattsface <spahmatthew@gmail.com>
2014-01-30 09:17:21 -0800

    updated calculator

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,23 +1,14 @@
 class Calculator
-
-	# method to check if args passed to method is a multidimensional array
-	def is_dim(var)
-		var.each do |x| 
-			true if x.is_a?(Array)
-		end 
-		false
-	end
-
+	
 	def sum(*args)
-		args.flatten! if is_dim(args) # flatten array if its multidimensional
+		args.flatten!
 		args.inject(0) {|sum, element| sum+element} 
 	end
 	
 	def multiply(*args)
-		args.flatten! if is_dim(args) # flatten array if its multidimensional
+		args.flatten!
 		args.inject(1) {|product, element| product*element}
 	end
-
 	# derp
 	def pow(x, y)
 		x ** y	
