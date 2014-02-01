443efb8f04c8a571b2ba95465bc05b9a63b1f022
Neil Woodward <drlctr@gmail.com>
2014-02-01 14:34:48 -0800

    Commit Week 4 Homework

--- week3/exercises/book_spec.rb
+++ week3/exercises/book_spec.rb
@@ -1,10 +1,31 @@
 require './book.rb'
 
 describe Book do
-  
-  it "should have a pages" do
-    book = Book.new
-    book.should respond_to "pages"
+
+  before(:each) do
+  	@book = Book.new
+  	@book.pages = 2222
+  	@book.title = "Fred"
+  end
+
+  context "#Pages"  do
+
+    it "should have pages" do
+      @book.should respond_to "pages"
+    end
+
+    it "should allow us to set the number of pages" do
+    	@book.pages.should eq 2222
+    end
+
+  end
+
+  context "#Title" do
+
+    it "Should set and get the title" do 
+    	@book.title.should eq "Fred"
+    end
+
   end
 
 end
