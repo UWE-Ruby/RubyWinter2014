c81bb7b9a7875bc1a911ac94af3995dfd312b8fe
Rukia Fahim <RukK86@gmail.com>
2014-02-05 21:40:36 -0800

    Add my answers homework week 4

--- week4/homework/worker_spec.rb
+++ week4/homework/worker_spec.rb
@@ -2,15 +2,19 @@ require "#{File.dirname(__FILE__)}/worker"
 
 describe Worker do
 
+    before do
+        @Worker = Worker.new
+    end
+
 	it "executes a block and returns a string" do
-		result = Worker.work do
+		result =@Worker.work do
 			"hello"
 		end
 		result.should == "hello"
 	end
 
 	it "executes a block and returns a number" do
-		result = Worker.work do
+		result =@Worker.work do
 			3 + 4
 		end
 		result.should == 7
@@ -18,7 +22,7 @@ describe Worker do
 
 	it "executes a block in the context of the calling method" do
 		n = 1
-		result = Worker.work do
+		result = @Worker.work do
 			n + 4
 		end
 		result.should == 5
@@ -27,7 +31,7 @@ describe Worker do
 
 	it "executes a block 3 times and returns the result" do
 		n = 5
-		result = Worker.work(3) do
+		result = @Worker.work(3) do
 			n += 1
 		end
 		result.should == 8
