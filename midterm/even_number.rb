44272bb277d01a9715de353c8861a4e9ef93ea29
keviny22 <kevin_young@intuit.com>
2014-02-20 15:23:45 -0800

    updates

--- midterm/even_number.rb
+++ midterm/even_number.rb
@@ -1,16 +1,23 @@
 class EvenNumber
 
   def next(num)
-    return false if not num.even?
+    return false unless num.even?
     num + 2
   end
 
   def compare(num1,num2)
-    return false if not num.even?
-    return false if not num.even?
+    return false unless num1.even?
+    return false unless num2.even?
+    true if num1 == num2
+  end
+  
+  def range(num1,num2)
+    return false unless num1.even?
+    return false unless num2.even?
+    (num1..num2).step(2) do |n|
+      n
+    end
+
   end
 
 end
-
-@evennumber = EvenNumber.new
-puts @evennumber.next(3)
--- midterm/even_number_spec.rb
+++ midterm/even_number_spec.rb
@@ -8,13 +8,28 @@
 require "#{File.dirname(__FILE__)}/even_number"
 
 describe EvenNumber do
-  
   before do
     @evennumber = EvenNumber.new
   end
   
   it "should only allow even numbers" do
-    @evennumber.should be_even
+    @evennumber.next(3).should be_false
+  end
+  
+  it "should return the next even number" do
+    @evennumber.next(2).should equal 4
+  end
+
+  it "should compare even numbers" do
+    @evennumber.compare(2,2).should be_true
+  end
+   
+  it "should not return true of numbers are not the same" do
+    @evennumber.compare(2,4).should_not be_true  
+  end
+
+  it "should generate a range of even numbers" do
+    @evennumber.range(2,8).should be_a Range
   end
 
 end
