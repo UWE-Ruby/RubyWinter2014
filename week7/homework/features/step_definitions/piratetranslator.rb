c7f9cda08e78f7b47c57cb2c22842345a9e84941
mattsface <spahmatthew@gmail.com>
2014-02-25 21:32:43 -0800

    homework

--- week7/homework/features/step_definitions/piratetranslator.rb
+++ week7/homework/features/step_definitions/piratetranslator.rb
@@ -1,12 +1,15 @@
 class PirateTranslator
 
+	# derp
 	def say(english)
 		@english = english
 	end
-
+	
+	# herp
 	def translate
 		 "Ahoy Matey\n Shiber Me Timbers You Scurvey Dogs!!"
 	end
 
 end
 
+	
