2e8876e8b027935bfdf1ea8c84cecceb6f7dfb3e
Rukia Fahim <RukK86@gmail.com>
2014-02-26 22:34:26 -0800

    Add passing pirate translator class

--- week7/homework/features/step_definitions/pirate_translator.rb
+++ week7/homework/features/step_definitions/pirate_translator.rb
@@ -1,3 +1,19 @@
 class PirateTranslator
+    attr_accessor :words, :translations
 
+    def initialize
+        @translations = Hash.new
+        @translations["Hello Friend"] = "Ahoy Matey\n Shiber Me Timbers You Scurvey Dogs!!"
+    end
+
+    def say words
+        @words = words
+    end
+
+    def translate
+        if @translations.has_key?(@words)
+            @translations[@words]
+        end
+    end
 end
+
