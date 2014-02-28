2e155b461309d15e7b3a143bf9bf196f2fdcbf62
Dan Williams <dwilliams@solutionsiq.com>
2014-02-28 08:48:43 -0800

    Update to week 7 homework pirate.rb file

--- week7/homework/features/step_definitions/pirate.rb
+++ week7/homework/features/step_definitions/pirate.rb
@@ -1,12 +1,26 @@
-class PirateTranslator
-	
-
-	def send(method, arg = 0)
-		@method = method
-		@arg = arg
-		@result = arg
-	end
-
-
-
+class String
+  def to_pirate_symbol
+    self.downcase.gsub(' ', '_').intern
+  end
 end
+
+ class PirateTranslator
+  PIRATE_WORDS = {
+    hello_friend: "Ahoy Matey"
+  }
+
+  def say something
+    @said = something
+  end
+
+  def translate
+    pirate_lookup + "\n Shiber Me Timbers You Scurvey Dogs!!"
+  end
+
+private
+
+  def pirate_lookup
+    key = @said.to_pirate_symbol
+    PIRATE_WORDS[key]
+  end
+ end
