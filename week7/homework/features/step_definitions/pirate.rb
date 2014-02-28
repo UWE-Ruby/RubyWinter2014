958c70659736256ea903627e6b1c16b884600467
Renée Hendricksen <renee@nird.us>
2014-02-27 17:49:55 -0800

    week7 homework answers

---
+++ week7/homework/features/step_definitions/pirate.rb
@@ -0,0 +1,26 @@
+class String
+  def to_pirate_symbol
+    self.downcase.gsub(' ', '_').intern
+  end
+end
+
+class PirateTranslator
+  PIRATE_WORDS = {
+    hello_friend: "Ahoy Matey"
+  }
+
+  def say something
+    @said = something
+  end
+
+  def translate
+    pirate_lookup(@said) + "\n Shiber Me Timbers You Scurvey Dogs!!"
+  end
+
+private
+
+  def pirate_lookup said
+    key = said.to_pirate_symbol
+    PIRATE_WORDS[key]
+  end
+end
