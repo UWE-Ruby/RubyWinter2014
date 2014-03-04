2bd7c54d2cb70f0d2dafe2f3df00848e232135d5
Rukia Fahim <RukK86@gmail.com>
2014-03-04 07:40:12 -0800

    Add current_player attribute and choose random first player

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,9 +1,11 @@
 class TicTacToe
 
-    attr_accessor :player
+    attr_accessor :player, :current_player
 
     def initialize
         @player = player
+
+        @current_player = [@player, "Computer"].sample
     end
 
     def welcome_player
