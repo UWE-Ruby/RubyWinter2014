6bacff475f40b65feaeb6dd088b4e66352ba875b
Rukia Fahim <RukK86@gmail.com>
2014-03-04 07:51:45 -0800

    First scenario passing

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,14 +1,25 @@
 class TicTacToe
 
-    attr_accessor :player, :current_player
+    attr_accessor :player, :comp, :current_player, :player_symbol, :computer_symbol
+
+    SYMBOLS = [:X, :O]
+
 
     def initialize
         @player = player
-
-        @current_player = [@player, "Computer"].sample
+        @comp = "Computer"
+        @current_player = [@player, @comp].sample
+        random_assign_symbols
     end
 
     def welcome_player
         "Welcome #{@player}"
+    end
+
+    private
+    def random_assign_symbols
+        symbols = SYMBOLS.shuffle # mix it up
+        @player_symbol = symbols.pop
+        @computer_symbol = symbols.pop
     end
 end
