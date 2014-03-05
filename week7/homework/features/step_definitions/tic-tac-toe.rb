c6feb613dfb9b128e8aaa1e21ba09b78463d49a1
Rukia Fahim <RukK86@gmail.com>
2014-03-04 19:35:06 -0800

    Now working on open spots method

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,9 +1,12 @@
 class TicTacToe
 
-    attr_accessor :player, :player_symbol, :computer_symbol, :players, :current_player
+    attr_accessor :player, :player_symbol, :computer_symbol, :players, :current_player, :board, :open
 
     SYMBOLS = [:X, :O]
     PAIRS = {:X => :O, :O => :X}
+    BOARD = {"A1" => nil, "A2" => nil, "A3" => nil,
+        "B1" => nil, "B2" => nil, "B3" => nil,
+        "C1" => nil, "C2" => nil, "C3" => nil}
 
 
     def initialize(starting_player=nil, starting_symbol=nil)
@@ -13,14 +16,10 @@ class TicTacToe
         if starting_symbol.nil?
             random_assign_symbols
         else
-            if @players[starting_player] == "Computer"     
-                @computer_symbol = starting_symbol
-                @player_symbol = PAIRS[starting_symbol]
-            else
-                @computer_symbol = PAIRS[starting_symbol]
-                @player_symbol = starting_symbol
-            end
+            @computer_symbol = PAIRS[starting_symbol]
+            @player_symbol = starting_symbol
         end
+        @board = BOARD
     end
 
     def welcome_player
@@ -33,9 +32,37 @@ class TicTacToe
 
     def get_player_move move=gets
         move = gets
+        if @board[move].nil?
+            @board[move] = get_current_player_symbol
+        end
+    end
+
+    def open_spots
+        @open = []
+        @board.each do |spot|
+            if @board[spot].nil?
+                @open << spot
+            end
+        end
+        @open
+    end
+
+    def computer_move
+        move = @open.sample
+        @board[move] = get_current_player_symbol
+        @open
     end
 
     private
+
+    def get_current_player_symbol
+        if @current_player == "Computer"
+            @computer_symbol
+        else
+            @player_symbol
+        end
+    end
+
     def random_assign_symbols
         symbols = SYMBOLS.shuffle # mix it up
         @player_symbol = symbols[0]
