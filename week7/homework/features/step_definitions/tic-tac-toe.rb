ed7b17d04575eb2ad451e59d4786bc81fc3ee17f
Rukia Fahim <RukK86@gmail.com>
2014-03-04 18:43:51 -0800

    Save progress thus far

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,28 +1,37 @@
 class TicTacToe
 
-    attr_accessor :player, :player_symbol, :computer_symbol
+    attr_accessor :player, :player_symbol, :computer_symbol, :players, :current_player
 
     SYMBOLS = [:X, :O]
+    PAIRS = [:X => :O, :O => :X]
 
 
-    def initialize(starting_player=nil)
-        @current_player = starting_player || [:player, :computer].sample
-        random_assign_symbols
+    def initialize(starting_player=nil, starting_symbol=nil)
+        @players = {:player => "Renee", :computer => "Computer"}
+        @current_player = @players[starting_player] || @players.values.sample
+
+        if starting_symbol.nil?
+            random_assign_symbols
+        else
+            if @players[starting_player] == "Computer"     
+                @computer_symbol = starting_symbol
+                @player_symbol = PAIRS[starting_symbol]
+            else
+                @computer_sumbol = PAIRS[starting_symbol]
+                @player_symbol = starting_symbol
+            end
+        end
     end
 
     def welcome_player
         "Welcome #{@player}"
     end
 
-    def current_player
-        {:player => @player, :computer => "Computer"}[@current_player]
-    end
-
     def indicate_player_turn
         "#{@current_player}'s move"
     end
 
-    def get_player_move
+    def get_player_move move=gets
         move = gets
     end
 
