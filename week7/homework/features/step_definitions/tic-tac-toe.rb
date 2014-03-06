9c08d60c7e2c2f49e56594826dd74594f9eb4dcc
Rukia Fahim <RukK86@gmail.com>
2014-03-05 22:12:18 -0800

    Now on third Scenario

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
 
-    attr_accessor :player, :player_symbol, :computer_symbol, :winner
+    attr_accessor :player, :player_symbol, :computer_symbol, :winner, :board
     SYMBOLS = [:X, :O]
 
     def initialize(current_player=nil, human_player_symbol=nil)
@@ -16,6 +16,10 @@ class TicTacToe
             @player_symbol = random_sym[0]
             @computer_symbol = random_sym[1]
         end
+        @board = {
+            :A1 => ' ', :A2 => ' ', :A3 => ' ',
+            :B1 => ' ', :B2 => ' ', :B3 => ' ',
+            :C1 => ' ', :C2 => ' ', :C3 => ' '}
         @winner = nil
     end
 
@@ -30,6 +34,30 @@ class TicTacToe
 
     def indicate_player_turn
         puts "#{player}'s Move:"
+    end
+
+    def open_spots
+        @open_spots = @board.select{|k, v| v == " "}.keys
+    end
+
+    def get_player_move
+        gets.chomp
+    end
+
+    def player_move
+        until open_spots.include?(move = get_player_move.to_sym)
+            indicate_player_turn
+        end
+        @board[move] = @player_symbol
+        @current_player = :computer
+        move
+    end
+
+    def computer_move
+        move = open_spots.sample
+        @board[move] = @computer_symbol
+        @current_player = :player
+        move
     end
 
     def over?
