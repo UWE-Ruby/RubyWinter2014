6a46ebaa90061718c10552cf263993062c9a20cd
Rukia Fahim <RukK86@gmail.com>
2014-03-06 00:11:20 -0800

    cucumber passing but play game is again skipping right to declaring winner

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,6 +1,6 @@
 class TicTacToe
 
-    attr_accessor :player, :player_symbol, :computer_symbol, :winner, :board
+    attr_accessor :player, :player_symbol, :computer_symbol, :board, :winner
     SYMBOLS = [:X, :O]
 
     def initialize(current_player=nil, human_player_symbol=nil)
@@ -25,7 +25,7 @@ class TicTacToe
     end
 
     def current_player
-        @players = {:player => @player, :computer => "Computer"}
+        @players = {:player => player, :computer => "Computer"}
         @players[@current_player]
     end
 
@@ -34,7 +34,7 @@ class TicTacToe
     end
 
     def indicate_player_turn
-        puts "#{player}'s Move:"
+        puts "#{current_player}'s Move:"
     end
 
     def open_spots
@@ -52,13 +52,16 @@ class TicTacToe
             indicate_player_turn
         end
         @board[move] = @player_symbol
+        determine_winner
         @current_player = :computer
         move
     end
 
     def computer_move
         move = open_spots.sample
+        indicate_player_turn
         @board[move] = @computer_symbol
+        determine_winner
         @current_player = :player
         move
     end
@@ -66,14 +69,59 @@ class TicTacToe
     def current_state
         state = ""
         @board.values.map { |value| value.to_s }.each_slice(3) { |row|
-            state << "#{row.to_s.gsub(",", "|")}\n"
+            state << "\n#{row.to_s.gsub!(',', '|')}\n"
         }
         state
     end
 
+    def determine_winner
+        # get each player's moves
+        computer_plays = @board.map{ |k,v| v==@computer_symbol ? k : nil }.compact
+        player_plays = @board.map{ |k,v| v==@player_symbol ? k : nil }.compact
+
+        winning_matches = {1 => [:A1, :A2, :A3], 2 => [:B1, :B2, :B3], 3 => [:C1, :C2, :C3], 4 => [:A1, :B1, :C1], 
+            5 => [:A2, :B2, :C2], 6 => [:A3, :B3, :C3], 7 => [:A1, :B2, :C3], 8 => [:A3, :B2, :C1]}
+        if winning_matches.has_key?(computer_plays)
+            @winner = :computer
+
+        elsif winning_matches.has_key?(player_plays)
+            @winner = :player
+        end
+        over?
+    end
+
+    def player_won?
+        if @winner = :player
+            true
+        end
+    end
+
+    def computer_won?
+        if @winner = :computer
+            true
+        end
+    end
+
+    def draw?
+        if @winner.nil? && !spots_open?
+            true
+        end
+    end
+
+    def spots_open?
+        open_spots.any?
+    end
+
     def over?
         #game is NOT over if @winner.nil and there are spots open
+
         #game is over if draw?, computer_won? or player_won? is true
+        if draw? || computer_won? || player_won?
+            true
+        else
+            false
+        end
+    
     end
 
 end
