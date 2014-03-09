70471dc9c8244c6d4d85c310434298ca94f4746a
Lynn Conway <lconner@bendcable.com>
2014-03-09 11:10:44 -0700

    Working MadLib game w/ debugging

--- final/MadLib/features/step_definitions/madlib_game.rb
+++ final/MadLib/features/step_definitions/madlib_game.rb
@@ -4,7 +4,11 @@ class MadLib
 		@word_array = []
 		@madlib_types = ["Pirate", "Student", "Vacation"]
 		puts "Which MadLib would you like to play?"
-		@madlib_types.each{|x| print x + ", "}
+		n = @madlib_types.length
+		for i in 0...(n-1)
+			print @madlib_types[i] + " or "
+		end
+		print @madlib_types[n-1] + "?  "
 	end
 
 	def get_madlib_type
@@ -38,15 +42,17 @@ class MadLib
 			this_line = line.split
 			this_line.each do |word| 
 				if  word =~ /@@@/
-					@word_array.push({word.sub(/@@@/, "") => ""})
+					word.sub!(/@@@/, "").sub!(/\W/, "")
+					@word_array.push({word => ""})
+					#@word_array.push({word.sub(/@@@/, "") => ""})
 				end 
 			end
 		end
 		#puts @word_array
-		# @word_array.each do |h|
-		# 	h.each {|k, v| puts "key is #{k}    value is #{v}"}
-		# end
-		#puts "exit collect_word_types"
+		@word_array.each do |h|
+			h.each {|k, v| puts "key is #{k}    value is #{v}"}
+		end
+		puts "exit collect_word_types"
 	end
 
 	def user_input
