0a9e9bd595bcc89af4b917cd1add5ef57562abde
John Wade <JohnRobertWade@gmail.com>
2014-03-10 19:17:27 -0700

    completed testing

--- gem_test/gem_test.rb
+++ gem_test/gem_test.rb
@@ -1,4 +1,4 @@
 require 'open_exchange_rates_conversion'
 
 new_currency = Converter.new
-	puts new_currency.currency_exchange('DKK', 2, '912735b01e8143bf8dee944d7673ed06')
+	puts new_currency.currency_exchange('USD', 1, '912735b01e8143bf8dee944d7673ed06')
