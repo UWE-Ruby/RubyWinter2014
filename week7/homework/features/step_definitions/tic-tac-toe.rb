2be722596592abace96ab3dff8d54ca9f0585a7e
kodywilson <kodywilson@gmail.com>
2014-03-13 09:46:00 -0700

    Both first and second scenario's passing now.

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -4,34 +4,42 @@ end
 
 class TicTacToe
   
-  attr_accessor :player, :current_player, :player_symbol, :computer_symbol
+  attr_accessor :player, :HAL, :current_player, :player_symbol, :computer_symbol
   
   SYMBOLS = [:X, :O]
   
-  def initialize(player="Bob")
-    @player = player
-  end
-  
-  def welcome_player
-    "Welcome #{@player}"
-  end
-  
-  def current_player
+  def initialize(player="Kody")
+    @player = "Kody"
+    @HAL = "Computer"
+    # now we determine who goes first
     player_roll = 0
     computer_roll = 0
     while player_roll == computer_roll
       player_roll = dice
       computer_roll = dice
     end
+    # now we assign symbols to each player
     if player_roll > computer_roll
       @player_symbol = :X
       @computer_symbol = :O
-      return "Kody"
+      @current_player = "#{@player}"
     else
       @player_symbol = :O
       @computer_symbol = :X
-      return "Computer"
+      @current_player = "#{@HAL}"
     end
+  end
+  
+  def welcome_player
+    "Welcome #{@player}"
+  end
+  
+  def indicate_player_turn
+    puts "#{@player}'s Move:"
+  end
+  
+  def get_player_move
+    STDIN.gets.chomp.upcase
   end
 
 end
